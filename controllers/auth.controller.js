eeecfdf1beff52477317b14322849a80083e4731
Nam <namdocsat@gmail.com>
2022-04-28 09:59:13 -0400

    add styling to base, form, auth, add connection to database, add functions to handle signup requests

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,13 @@
-function getSignup(req, res, next) {}
+function getSignup(req, res, next) {
+  res.render("customer/auth/signup");
+}
+
+function signup(req, res) {}
 
 function getLogin(req, res, next) {}
 
 module.exports = {
   getSignup: getSignup,
   getLogin: getLogin,
+  signup: signup,
 };
